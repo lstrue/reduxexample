36317ce1dfd5b3add8659c87a9d24b08fbef9ec7
David Li <shuai_li@neimanmarcus.com>
2019-04-25 18:08:55 -0500

    Add redux store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Provider} from 'react-redux';
 
 import Posts from './components/Posts';
 import Postform from './components/Postform';
+import {createStore, applyMiddleware } from 'redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        Welcome to React
-      </header>
-      <Postform />
-      <hr />
-      <Posts />
-    </div>
-  );
+const store = createStore(() => [], {}, applyMiddleware);
+
+class App extends Component {
+  render(){
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            Welcome to React
+          </header>
+          <Postform />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
